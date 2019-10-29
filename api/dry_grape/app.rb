bc74d9c5ed9870b8f19e861ed2fd01f3b9d7bf06
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-10-29 20:35:42 +0100

    Basic endpoints

--- api/dry_grape/app.rb
+++ api/dry_grape/app.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class DryGrape::App < Grape::API
-  prefix 'api'
-
   format :json
+
+  get '/' do
+    {}
+  end
+
+  get '/ping' do
+    { ping: 'pong' }
+  end
+
+  route :any, '*path' do
+    error!({ error: 'Not found' }, 404)
+  end
 end
